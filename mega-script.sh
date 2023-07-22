6acabf2d8885ca5e33b0a7dd1c2a2589650302e0
Gonzalo Lombardi <37313781+GonzaloLombardi@users.noreply.github.com>
2023-07-22 04:03:01 -0300

    Create mega-script.sh

---
+++ mega-script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo  "Starting script"
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'This script must be run by root' >&2
+        exit 1
+fi
+echo "Set up the repository"
+apt-get update
+apt-get install ca-certificates curl gnupg
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "Install Docker Engine"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+echo "Linux post-installation steps for Docker Engine"
+usermod -aG docker $SUDO_USER
+apt install qemu-guest-agent -y
+file_url="https://raw.githubusercontent.com/GonzaloLombardi/scripts/main/ssh/motd"
+wget "$file_url" -O /tmp/motd
+mv /tmp/motd /etc/
+rm -rf /etc/update-motd.d/*
+apt-get install nano
+reboot
