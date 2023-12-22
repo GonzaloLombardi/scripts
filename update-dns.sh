34dae35b4e2231f76149ba2fc15e00460a09e81e
Gonzalo Lombardi <37313781+GonzaloLombardi@users.noreply.github.com>
2023-12-21 23:29:25 -0300

    Create update-dns.sh

---
+++ update-dns.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo  "Starting script"
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'This script must be run by root' >&2
+        exit 1
+fi
+dns="DNS=192.168.4.1"
+sed -i '$s/.$//' "/etc/systemd/resolved.conf"
+echo "$nameservers" >> "/etc/systemd/resolved.conf"
+resolvectl flush-caches
+service systemd-resolved restart
