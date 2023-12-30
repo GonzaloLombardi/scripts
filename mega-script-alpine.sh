4b5d1b9fcfacb2098d0ef55b5674f32e8f0f355f
Gonzalo Lombardi <37313781+GonzaloLombardi@users.noreply.github.com>
2023-12-30 00:42:58 -0300

    Update mega-script-alpine.sh

--- mega-script-alpine.sh
+++ mega-script-alpine.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 echo  "Starting script"
 apk update
+echo "Installing OpenSSH"
 apk add openssh
 rc-update add sshd
 service sshd start
+echo "Installing Nano"
 apk add nano
+echo "Installing Docker"
 apk add docker docker-compose
 rc-update add docker default
 /etc/init.d/docker start
+echo "Changing MOTD"
 file_url="https://raw.githubusercontent.com/GonzaloLombardi/scripts/main/ssh/motd"
 wget "$file_url" -O /tmp/motd
 rm /etc/motd
